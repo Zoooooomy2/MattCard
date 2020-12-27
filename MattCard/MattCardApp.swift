a9477ffe46a1d8454fdfcc99db1ddfb62aed9a3c
Matt Nutt <mattbnutt@sky.com>
2020-12-27 13:05:27 +0000

    Initial Commit

---
+++ MattCard/MattCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MattCardApp.swift
+//  MattCard
+//
+//  Created by Matt Nutt on 27/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MattCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
